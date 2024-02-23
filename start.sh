a0e3ddd74a7ebaad00485a2bde49dae802113f58
observer1220 <observer1220@gmail.com>
2024-02-23 17:38:24 +0800

    fix dockerfile #4

--- start.sh
+++ start.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-echo "run db migration"
-source /app/app.env
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+echo "Run db migration"
+echo "DB_SOURCE: $DB_SOURCE"
+
+# FTL cannot create new migrate instance error="pq: the database system is starting up"
+# /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
+# FTL cannot create new migrate instance error="pq: password authentication failed for user \"root\""
 
 echo "start the app"
 exec "$@"
